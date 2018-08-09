35e490ef2662a76ff60dd87ec9d74f57f56d4621
Sahal Sajjad <sahalsajjad@gmail.com>
2018-08-09 17:28:12 +0530

    Added headers

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,2 @@
-export const methods =  {
-    GET:"GET",
-    POST:"POST",
-    PUT:"PUT",
-    DELETE:"DELETE",
-    HEAD:"HEAD",
-    CONNECT:"CONNECT",
-    OPTIONS:"OPTIONS",
-    TRACE:"TRACE"
-}
+export {methods} from './methods';
+export {headers} from './headers';
---
+++ src/methods.ts
@@ -0,0 +1,10 @@
+export const methods =  {
+    GET:"GET",
+    POST:"POST",
+    PUT:"PUT",
+    DELETE:"DELETE",
+    HEAD:"HEAD",
+    CONNECT:"CONNECT",
+    OPTIONS:"OPTIONS",
+    TRACE:"TRACE"
+}
